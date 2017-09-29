14ae7ef7048b667dcffd09844574bc69f2de4248
krissycervan <krissycervan@gmail.com>
2017-09-29 18:59:24 +0000

    flaskerror

--- flask_redirect.py
+++ flask_redirect.py
@@ -3,8 +3,9 @@ app = Flask(__name__)
 
 @app.route("/private")
 def private():
-  #Test for user logged in failed
-  #so redirect to log in URL
+#Test for user logged in failed
+
+#so redirect to login URL
 
  return redirect(url_for('login'))
 
