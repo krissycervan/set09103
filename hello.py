70e377ff027e03b1ea0646e63fe6f9a85e3016f8
krissycervan <krissycervan@gmail.com>
2017-09-29 17:43:52 +0000

    commit

--- hello.py
+++ hello.py
@@ -2,9 +2,17 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-  return "Hello World!"
+def root():
+ return "the default, 'root' route"
 
-  if __name__=="__main__":
-    app.run(host='0.0.0.0')
+@app.route("/hello/")
+def hello():
+  return "Hello Napier!!! :D"
+
+@app.route("/goodbye/")
+def goodbye():
+ return "Goodbye cruel world :("
+
+if __name__=="__main__":
+ app.run(host='0.0.0.0', debug=True)
 
